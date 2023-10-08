402831ae177b376fd84545f6ef4a1be156cdceec
StarpowerNaledi <n.mokua972@gmail.com>
2023-10-08 10:07:26 -0700

    Update source code

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,16 +6,10 @@
 
 int main(void)
 {
-	/* Declare variables
-	 * to be used in code*/
 	char c, b;
 
-	/*Function and statement that print in lowercase
-	 *c++ is an increment funtion */
 	for (c = 'a'; c <= 'z'; c++)
 		putchar(c);
-	/*Function and statement that print in uppercase
-	 *b++ is an increment funtion */
 	for (b = 'A'; b <= 'Z'; b++)
 		putchar(b);
 	putchar('\n');
