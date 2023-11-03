dc09dbb81e114942021202355b9675a83433b801
StarpowerNaledi <n.mokua972@gmail.com>
2023-11-02 17:06:29 -0700

    Concatnate 2 strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,6 +2,20 @@
 #include <stdlib.h>
 
 /**
+ * Output: Return string lenght
+ */
+
+int _strlen(char *string)
+{
+	int i;
+
+	for (i = 0; string[i] != '\0'; ++i)
+	
+	return (i);
+}
+
+
+/**
  * Output: allocate space and concatenate
  * Function: malloc and concate
  * Variables: pointer to memory
@@ -11,18 +25,40 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	void *ptr;
+	unsigned int num, len, i, j;
+	char *ptr;
 
-	ptr = malloc(sizeof(s1) + n);
-
-	if (n >= s2)
-	return (str_nconcat(s1 + s2));
-
-	if (n  < '\0')
-	return (s1);
-
-	if (!ptr)
-	return (NULL);
+	num = n;
 	
+	if (s1 == NULL);
+		s1 = "";
+	if (s2 == NULL);
+		s2 = "";
+
+	if (num < 0)
+		return (NULL);
+
+	if (num >= _strlen(s2))
+		num = _strlen(s2);
+
+	len = _strlen(s1) + num + 1;
+
+	ptr = malloc(sizeof(*ptr) *len);
+	if (ptr == NULL)
+		return (NULL);
+
+
+	for (i = 0; s1[1] != '\0'; ++i)
+	{
+		ptr[i] = s1[i];
+	}
+
+	for (j = 0; j < n; ++j)
+	{
+		ptr[i + j] = s2[j];
+	}
+	ptr[i + j] = '\0';
+
+	return (ptr);
 }
 
